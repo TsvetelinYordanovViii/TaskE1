62030ec9975050f44df19ef2d2913b7e141835ba
Tsvetelin Yordanov <tsvetelin_yordanov_viii@abv.bg>
2023-09-05 18:46:08 +0300

    Offers and latest offers now slide in when scrolling down.

--- Scripts/ScrollAnimations.js
+++ Scripts/ScrollAnimations.js
@@ -1,5 +1,57 @@
 const controller = new ScrollMagic.Controller();
 
-const slideLeftNav = new ScrollMagic.Scene({
-    triggerElemenent: '.offers'
-})
+const slideOfferSlider = new ScrollMagic.Scene(
+    {
+        triggerElement: '.offers',
+        triggerHook: .7,
+        offset: -150
+    })
+    .setTween(TweenMax.from('.offer-slider', 1, {
+        x: '225%',
+        ease: Power0.ease
+    }))
+    .addIndicators()
+    .addTo(controller);
+
+
+const slideOfferCard = new ScrollMagic.Scene(
+    {
+        triggerElement: '.offers',
+        triggerHook: .7,
+        offset: -150
+    })
+    .setTween(TweenMax.from('.offer-card', 1, {
+        x: '-225%',
+        ease: Power0.ease
+    }))
+    .addIndicators()
+    .addTo(controller);
+
+
+
+const slideLatestSlider = new ScrollMagic.Scene(
+    {
+        triggerElement: '.latest',
+        triggerHook: .7,
+        offset: -150
+    })
+    .setTween(TweenMax.from('.latest-slider', 1, {
+        x: '-225%',
+        ease: Power0.ease
+    }))
+    .addIndicators()
+    .addTo(controller);
+
+
+const slideAuctionCard = new ScrollMagic.Scene(
+    {
+        triggerElement: '.latest',
+        triggerHook: .7,
+        offset: -150
+    })
+    .setTween(TweenMax.from('.auction-card', 1, {
+        x: '225%',
+        ease: Power0.ease
+    }))
+    .addIndicators()
+    .addTo(controller);
